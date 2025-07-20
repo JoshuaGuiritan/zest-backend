866dcb57d32dc11842b411bb77567182659a2723
Joshua Guiritan <joshuaguiritan05@gmail.com>
2025-07-20 15:34:59 +0800

    first commit

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import router from "../routes/route.js";
 import dbConnect from "../db/dbconn.js";
-import serverless from "serverless-http";
 import cors from "cors";
 
 dbConnect();
@@ -12,5 +11,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
 
-export default serverless(app);
+export default app;
 
